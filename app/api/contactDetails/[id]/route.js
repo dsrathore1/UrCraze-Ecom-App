ff9dba5a7552abe0fca5ac636e580327a2891051
DS <dsrathorebsw1234@gmail.com>
2023-12-14 18:00:43 +0530

    Created Update API by ID

--- app/api/contactDetails/[id]/route.js
+++ app/api/contactDetails/[id]/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import client from "@/helper/DB/connectDB";
-import { deleteDetailByID, getContactDataByID } from "@/helper/Queries/query";
+import { deleteDetailByID, getContactDataByID, updateDetailByID } from "@/helper/Queries/query";
 
 export async function GET(req, { params }) {
     const getDataById = await client.query(getContactDataByID, [params.id]);
@@ -12,4 +12,10 @@ export async function GET(req, { params }) {
 export async function DELETE(req, { params }) {
     await client.query(deleteDetailByID, [params.id]);
     return NextResponse.json("Your given ID's data is deleted successfully");
+}
+
+export async function PUT(req, { params }) {
+    const { name, email, msg } = await req.json();
+    await client.query(updateDetailByID, [name, email, msg, params.id]);
+    return NextResponse.json({ message: "Your data is Updated"});
 }
--- helper/Queries/query.js
+++ helper/Queries/query.js
@@ -2,4 +2,5 @@ export const getContactDetail = 'SELECT * FROM CONTACT_DETAILS';
 export const insertData = 'INSERT INTO CONTACT_DETAILS(NAME, EMAIL, MSG) VALUES($1,$2,$3)';
 export const getContactDataByID = 'SELECT * FROM CONTACT_DETAILS WHERE ID=$1';
 export const checkEmailExists = 'SELECT * FROM CONTACT_DETAILS WHERE EMAIL=LOWER($1) OR EMAIL=UPPER($1)';
-export const deleteDetailByID='DELETE FROM CONTACT_DETAILS WHERE ID=$1';
+export const deleteDetailByID = 'DELETE FROM CONTACT_DETAILS WHERE ID=$1';
+export const updateDetailByID = 'UPDATE CONTACT_DETAILS SET NAME=$1, EMAIL=$2, MSG=$3 WHERE ID=$4';
