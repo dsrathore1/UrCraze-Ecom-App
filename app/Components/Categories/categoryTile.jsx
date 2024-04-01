4e3c0b1c77250ea1c3c28146d3d73c697164a6e6
DS <dsrathorebsw1234@gmail.com>
2024-04-01 16:01:25 +0530

    Modified some files

--- app/Components/Categories/categoryTile.jsx
+++ app/Components/Categories/categoryTile.jsx
@@ -1,17 +1,23 @@
-const CategoryTile = ({ icon, category_name, active, on_Click }) => {
+import { useState } from "react";
 
-    const activeClass = 'flex-col flex justify-center items-start w-[12rem] h-[8rem] px-5 rounded-3xl text-gray-200 text-[#efdf4e] bg-[#fff4ae50] border-[#decc2a] border-[4px]';
+const CategoryTile = ({ icon, category_name }) => {
+  const activeClass =
+    "flex-col flex justify-center items-start w-[12rem] h-[8rem] px-5 rounded-3xl text-[#efdf4e] bg-[#fff4ae50] border-[#decc2a] border-[4px]";
 
-    const non_ActiveClass = 'flex-col flex justify-center items-start border-[2px] w-[12rem] border-white/30 h-[8rem] px-5 rounded-3xl text-gray-200 hover:text-[#efdf4e] hover:bg-[#fff4ae50] hover:border-[#decc2a] hover:border-[4px]';
+  const non_ActiveClass =
+    "flex-col flex justify-center items-start border-[2px] w-[12rem] border-white/30 h-[8rem] px-5 rounded-3xl text-gray-200 hover:text-[#fff] hover:bg-[#fff4ae50] hover:border-[#decc2a] hover:border-[4px]";
 
-    return (
-        <div className={active ? activeClass : non_ActiveClass} onClick={on_Click}>
-            <div className='mb-3'>
-                {icon}
-            </div>
-            <span className='capitalize text-xl'>{category_name}</span>
-        </div>
-    )
-}
+  const [active, setActive] = useState(false);
 
-export default CategoryTile
+  return (
+    <div
+      className={active ? activeClass : non_ActiveClass}
+      onClick={() => setActive(!active)}
+    >
+      <div className="mb-3">{icon}</div>
+      <span className="capitalize text-xl">{category_name}</span>
+    </div>
+  );
+};
+
+export default CategoryTile;
--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -1,91 +1,161 @@
-"use client"
-import Link from 'next/link';
-import { Kaushan_Script } from 'next/font/google';
-import { BsGithub, BsSearch, BsTwitter, BsInstagram } from "react-icons/bs"
-import { useState, useEffect, useRef } from 'react';
+"use client";
+import Link from "next/link";
+import { Kaushan_Script } from "next/font/google";
+import { BsGithub, BsSearch, BsTwitter, BsInstagram } from "react-icons/bs";
+import { useState, useEffect, useRef } from "react";
+import { usePathname } from "next/navigation";
 
 const k_s = Kaushan_Script({
-    style: ["normal"],
-    subsets: ["latin"],
-    weight: ["400"]
-})
+  style: ["normal"],
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 const Navbar = () => {
-    const [click, setClick] = useState(true);
+  const [click, setClick] = useState(true);
 
-    const searchBtnClicked = () => {
-        setClick(false);
-    }
+  const pathname = usePathname();
 
-    const ref = useRef(null);
+  const searchBtnClicked = () => {
+    setClick(false);
+  };
 
-    useEffect(() => {
-        const handleOutSideClick = (event) => {
-            if (!ref.current?.contains(event.target)) {
-                setClick(true);
-            }
-        };
+  const ref = useRef(null);
 
-        window.addEventListener("mousedown", handleOutSideClick);
+  useEffect(() => {
+    const handleOutSideClick = (event) => {
+      if (!ref.current?.contains(event.target)) {
+        setClick(true);
+      }
+    };
 
-        return () => {
-            window.removeEventListener("mousedown", handleOutSideClick);
-        };
-    }, [ref]);
+    window.addEventListener("mousedown", handleOutSideClick);
 
-    return (
-        <nav className="bg-[#000] flex flex-col w-full z-50">
-            <div className='bg-[#181818] flex justify-center items-center h-12'>
-                Black Friday. <span className='text-amber font-bold'>Save upto 50%</span>
-                <button className='border-2 mx-10 border-amber py-[5px] px-6'>Learn More</button>
-            </div>
-            <div className='flex items-center h-[20vh] justify-between relative px-5'>
-                <ul className='flex justify-center items-center gap-5'>
-                    <BsTwitter className='text-2xl' />
-                    <BsGithub className='text-2xl' />
-                    <BsInstagram className='text-2xl' />
-                </ul>
-                <Link className='flex flex-col ml-44 justify-center items-center' href="/">
-                    <p className={`${k_s.className} transition-all ease-in duration-200 text-white hover:text-amber text-6xl font-bold cursor-pointer`}><span className='text-white'>Ur</span>Craze</p>
-                    <p className={`${k_s.className} transition-all ease-in duration-200 hover:text-amber`}>multipurpose shopping hub</p>
-                </Link>
-                {/* Search Bar */}
-                <div className={`p-2 w-[25vw] border-solid border-2 rounded flex items-center space-x-4 ${click ? "border-white" : "border-amber"}`}>
-                    <BsSearch className={`text-4xl ${click ? "text-white" : "text-amber"}`} />
-                    <input
-                        onClick={searchBtnClicked}
-                        type="text"
-                        placeholder="Search..."
-                        className={`w-[40vw] bg-transparent border-none outline-none text-white`}
-                        ref={ref}
-                    />
-                </div>
-            </div>
+    return () => {
+      window.removeEventListener("mousedown", handleOutSideClick);
+    };
+  }, [ref]);
 
-            {/* Navigation Links */}
-            <div className='border-t-[.5px] w-full flex justify-center items-center h-[8vh] border-gray-200 border-opacity-20 py-5'>
-                <div className="flex items-center space-x-7 font-medium">
-                    <Link href="/">
-                        <p className="text-lg cursor-pointer text-amber">Home</p>
-                    </Link>
-                    <Link href="/about">
-                        <p className="text-white transition-all duration-200 ease-in-out text-lg cursor-pointer hover:text-amber">About Us</p>
-                    </Link>
-                    <Link href="/cart">
-                        <p className="text-white transition-all duration-200 ease-in-out text-lg hover:text-amber">Cart</p>
-                    </Link>
-                    <Link href="/contactUs">
-                        <p className="text-white transition-all duration-200 ease-in-out text-lg cursor-pointer hover:text-amber">Contact Us</p>
-                    </Link>
+  return (
+    <nav className="bg-[#000] flex flex-col w-full z-50">
+      <div className="bg-[#181818] flex justify-center items-center h-12">
+        Black Friday.{" "}
+        <span className="text-amber font-bold">Save upto 50%</span>
+        <button className="border-2 mx-10 border-amber py-[5px] px-6">
+          Learn More
+        </button>
+      </div>
+      <div className="flex items-center h-[20vh] justify-between relative px-5">
+        <ul className="flex justify-center items-center gap-5">
+          <BsTwitter className="text-2xl" />
+          <BsGithub className="text-2xl" />
+          <BsInstagram className="text-2xl" />
+        </ul>
+        <Link
+          className="flex flex-col ml-44 justify-center items-center"
+          href="/"
+        >
+          <p
+            className={`${k_s.className} transition-all ease-in duration-200 text-white hover:text-amber text-6xl font-bold cursor-pointer`}
+          >
+            <span className="text-white">Ur</span>Craze
+          </p>
+          <p
+            className={`${k_s.className} transition-all ease-in duration-200 hover:text-amber`}
+          >
+            multipurpose shopping hub
+          </p>
+        </Link>
+        {/* Search Bar */}
+        <div
+          className={`p-2 w-[25vw] border-solid border-2 rounded flex items-center space-x-4 ${
+            click ? "border-white" : "border-amber"
+          }`}
+        >
+          <BsSearch
+            className={`text-4xl ${click ? "text-white" : "text-amber"}`}
+          />
+          <input
+            onClick={searchBtnClicked}
+            type="text"
+            placeholder="Search..."
+            className={`w-[40vw] bg-transparent border-none outline-none text-white`}
+            ref={ref}
+          />
+        </div>
+      </div>
 
-                    <Link href="#">
-                        <p className='text-white transition-all duration-200 ease-in-out text-lg cursor-pointer hover:text-amber'>Account</p>
-                    </Link>
-                    <button className='mx-10 bg-amber px-8 py-2 rounded-sm shadow-md text-black font-semibold' onClick={() => { window.location.href = '/login' }}>Login</button>
-                </div>
-            </div>
-        </nav>
-    );
+      {/* Navigation Links */}
+      <div className="border-t-[.5px] w-full flex justify-center items-center h-[8vh] border-gray-200 border-opacity-20 py-5">
+        <div className="flex items-center space-x-7 font-medium">
+          <Link href="/">
+            <p
+              className={
+                pathname === "/"
+                  ? "text-lg cursor-pointer text-amber"
+                  : "text-white transition-all duration-200 ease-in-out text-lg cursor-pointer hover:text-amber"
+              }
+            >
+              Home
+            </p>
+          </Link>
+          <Link href="/about">
+            <p
+              className={
+                pathname === "/about"
+                  ? "text-lg cursor-pointer text-amber"
+                  : "text-white transition-all duration-200 ease-in-out text-lg cursor-pointer hover:text-amber"
+              }
+            >
+              About Us
+            </p>
+          </Link>
+          <Link href="/cart">
+            <p
+              className={
+                pathname === "/cart"
+                  ? "text-lg cursor-pointer text-amber"
+                  : "text-white transition-all duration-200 ease-in-out text-lg cursor-pointer hover:text-amber"
+              }
+            >
+              Cart
+            </p>
+          </Link>
+          <Link href="/contactUs">
+            <p
+              className={
+                pathname === "/contactUs"
+                  ? "text-lg cursor-pointer text-amber"
+                  : "text-white transition-all duration-200 ease-in-out text-lg cursor-pointer hover:text-amber"
+              }
+            >
+              Contact Us
+            </p>
+          </Link>
+
+          <Link href="/accounts">
+            <p
+              className={
+                pathname === "/accounts"
+                  ? "text-lg cursor-pointer text-amber"
+                  : "text-white transition-all duration-200 ease-in-out text-lg cursor-pointer hover:text-amber"
+              }
+            >
+              Accounts
+            </p>
+          </Link>
+          <button
+            className="mx-10 bg-amber px-8 py-2 rounded-sm shadow-md text-black font-semibold"
+            onClick={() => {
+              window.location.href = "/login";
+            }}
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
